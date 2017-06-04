4d4f44d535492803746db9238135191ce60fda50
Ahmed <ahmed.opeyemi@gmail.com>
2017-06-03 21:27:35 -0700

    Allow alphbets in boxes, fix classname toggle

--- index.js
+++ index.js
@@ -5,13 +5,19 @@ var PLAYER_MODES = {
 }
 
 var PLAYER_TILES = {
-    "PLAYER_ONE": "1",
-    "PLAYER_TWO": "2"
+    PLAYER_ONE: {
+        code: "1",
+        class: "player-one-color"
+    },
+    PLAYER_TWO: {
+        code: "2",
+        class: "player-two-color"
+    }
 }
 
 var selectedPlayerMode = null
 var activePlayer = PLAYER_TILES.PLAYER_ONE
-var boxOnHold = null
+var boxOnHoldClassName = ""
 
 // Utils
 function showInstruction(text) {
@@ -22,14 +28,18 @@ function clearInstruction() {
     document.getElementsByClassName("instruction")[0].innerText = ""
 }
 
-function isBoxEmpty(box) {
+function isBoxEmpty(box, acknowledgeQuestionMark) {
     clearInstruction()
-    if (!box.innerText) {
+    if (!box.innerText || (acknowledgeQuestionMark === true && box.innerText === "?")) {
         return true
     } else {
         showInstruction("You can't play on a tile a player has played on.")
         return false
     }
+}
+
+function getBox(className) {
+    return className ? document.getElementsByClassName(className)[0] : {}
 }
 
 /**
@@ -45,47 +55,52 @@ function switchPlayerMode(value) {
  * @param {element} box: The src element or box to be marked
  */
 function displayAlphabetOnBox(box, alphabet) {
-    if (isBoxEmpty(box)) {
-        box.innerText = alphabet
+    var activeBox = box
+    if (isBoxEmpty(activeBox, true)) {
+        activeBox.innerText = alphabet
         activePlayer = activePlayer === PLAYER_TILES.PLAYER_ONE
             ? PLAYER_TILES.PLAYER_TWO
             : PLAYER_TILES.PLAYER_ONE
-        showInstruction("Player "+ activePlayer +", please click on a tile")
+        clearBoxOnHold()
+        showInstruction("Player "+ activePlayer.code +", please click on a tile")
     }
 }
 
-function holdBox(box) {
-    if (isBoxEmpty(box)) {
-        if (boxOnHold) {
-            // clear previous box on hold if any
-            boxOnHold.innerText = ""
-            boxOnHold = null
+function holdBox(className) {
+    var box = getBox(className)
+    if (isBoxEmpty(box, false)) {
+        if (boxOnHoldClassName) {
+            var currentlyHeldBox = getBox(boxOnHoldClassName)
+            currentlyHeldBox.innerText = ""
+            // This sucks, do it in a better way.
+            currentlyHeldBox.className = currentlyHeldBox.className.replace(PLAYER_TILES.PLAYER_ONE.class, "").trim()
+            currentlyHeldBox.className = currentlyHeldBox.className.replace(PLAYER_TILES.PLAYER_TWO.class, "").trim()
         }
-         var playerClassName = activePlayer === PLAYER_TILES.PLAYER_ONE
-            ? "player-one-color"
-            : "player-two-color"
-        boxOnHold = box
+        clearBoxOnHold()
+        var playerClassName = activePlayer.class
+        var boxOnHold = box
         boxOnHold.innerText = "?"
         boxOnHold.className = boxOnHold.className.concat(" " + playerClassName)
-        showInstruction("Player "+ activePlayer + ", please enter your alphabet")
+        boxOnHoldClassName = className
+        showInstruction("Player "+ activePlayer.code + ", please enter your alphabet")
     }
 }
 
 function getBoxOnHold() {
     // looks simple now, but we might want to do
     // some more checks to determine if the box is on hold in future
-    return boxOnHold === null ? false : boxOnHold
+    return getBox(boxOnHoldClassName)
 }
 
 function clearBoxOnHold() {
-    
+    // clear previous box on hold if any
+    boxOnHoldClassName = ""
 }
 
 function onKeyPress(event) {
-    console.log("key is pressed...", event)
     var boxOnHold = getBoxOnHold()
     if (!boxOnHold) {
-        showInstruction("Player "+ activePlayer +", please click on a tile")
+        showInstruction("Player "+ activePlayer.code +", please click on a tile")
         return
     }
     displayAlphabetOnBox(boxOnHold, event.key.toUpperCase())
@@ -98,7 +113,8 @@ function init() {
     var columns = document.getElementsByClassName("game-board-col")
     for (var i = 0; i < columns.length; i++) {
         columns[i].onclick = function (event) {
-            holdBox(event.srcElement)
+            // holdBox(event.srcElement)
+            holdBox(event.srcElement.className)
         }
     }
     showInstruction("Click on a tile/box to begin")
